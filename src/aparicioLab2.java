72606323044e5638ab8daf49fe90ad3df91c38c1
KevnAp <apariciodkevin@Gmail.com>
2026-02-11 13:01:58 -0600

    Tested to see if 2d array structure printed correctly.

--- src/aparicioLab2.java
+++ src/aparicioLab2.java
@@ -7,5 +7,19 @@ public class aparicioLab2 {
 
         System.out.println("Please enter the odd number only!");
         System.out.println("Enter the square size: ");
+
+        // Use user input to create the rows and columns of the 2d array
+        int size = sc.nextInt();
+
+        // initialize 2d array
+        int[][] magicSquare = new int [size] [size];
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                System.out.print(magicSquare[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
