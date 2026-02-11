319585686986aee598d240ac683a751500aeecf5
KevnAp <apariciodkevin@Gmail.com>
2026-02-11 12:25:14 -0600

    Initiated Scanner and began coding prompt questions for user interaction.

--- src/aparicioLab2.java
+++ src/aparicioLab2.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class aparicioLab2 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the odd number only!");
+        System.out.println("Enter the square size: ");
     }
 }
