2af8e2fd35fa3d6671720ea2e72d39f4f1388dd0
KevnAp <apariciodkevin@Gmail.com>
2026-02-17 01:02:36 -0600

    Rebuilt the foundation for the program. Added user input validation.

--- src/aparicioLab2.java
+++ src/aparicioLab2.java
@@ -1,4 +1,23 @@
 import java.util.Scanner;
 
 public class aparicioLab2 {
+    public static void main(String[] args) {
+        // Scanner will read in input.
+        Scanner sc = new Scanner(System.in);
 
+        // Asking the user to enter an odd number. This will determing the
+        // size of the square
+        System.out.println("Please enter Odd number Only!");
+        int size = sc.nextInt();
+
+        // Checks to make sure user input is valid.
+        while (size % 2 == 0){
+            System.out.println("Please enter ODD NUMBER ONLY");
+            size = sc.nextInt();
+        }
+
+        // Builds two dimensional array to fit the size of the user input.
+
+
+    }
+}
