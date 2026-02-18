150176a6892e983f0f5e603531adf736cfcfafcb
KevnAp <apariciodkevin@Gmail.com>
2026-02-17 23:07:48 -0600

    restructured logic base again to display the array structure first.

--- src/aparicioLab2.java
+++ src/aparicioLab2.java
@@ -16,28 +16,14 @@ public class aparicioLab2 {
             size = sc.nextInt();
         }
 
-        // Builds two dimensional array to fit the size of the user input.
-        int [][] magicSquare = new int[size][size];
+        int [][] magicSquare = new int[size][size]; // initialize 2d array
 
-        int row = 0; // Starting position of the magic square
-        int newRow; // where the next row will move to
 
-        // We divide by two because in integer division that will bring me to the middle position.
-        int col = size / 2; // column
-        int newcol; // where the next column will move to
 
-        int num = 1; // Setting the first number in the square to 1
-
-        // First loop
-        for (int i = 0; i < size * size; i++) {
-            magicSquare[row][col] = num;
-            System.out.println(magicSquare[row][col]);
-
-            // add 1 to each iteration
-            num += 1;
-
-            if (magicSquare[row][col] > 0) { // Checks to see if the spot in the array is already filled.
-                newRow = row + 1; // moves down one.
+        for (int i = 0; i < size; i++){ // Displays contents of 2d array
+            System.out.println();
+            for (int j = 0; j < size; j++){
+                System.out.print(magicSquare[i][j]);
             }
         }
     }
